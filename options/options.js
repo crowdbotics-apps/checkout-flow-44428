692a3df1ffc462cf6a9974757a2db6724cc949e6
Crowdbotics <team@crowdbotics.com>
2023-11-08 23:10:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "checkout-flow-44428",
+  url: "https://checkout-flow-44428.botics.co",
+  api: "https://checkout-flow-44428.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
